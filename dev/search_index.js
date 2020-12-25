94f7bb993ed485127441729ee504ecf48c755e9a
Documenter.jl <documenter@juliadocs.github.io>
2020-12-25 11:25:52 +0000

    build based on 0ce9fdc

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Garamon","category":"page"},{"location":"#Garamon","page":"Home","title":"Garamon","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Garamon]","category":"page"}]
+}
