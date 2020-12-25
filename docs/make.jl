0ce9fdc58d91bd7c1ec6561764895d4ca017dc42
Azzaare <jf@baffier.fr>
2020-12-25 20:24:27 +0900

    Basic setup

--- docs/make.jl
+++ docs/make.jl
@@ -7,9 +7,9 @@ makedocs(;
     repo="https://github.com/JuliaConstraints/Garamon.jl/blob/{commit}{path}#L{line}",
     sitename="Garamon.jl",
     format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
+        prettyurls=get(ENV, "CI", nothing) == "true",
         canonical="https://JuliaConstraints.github.io/Garamon.jl",
-        assets=String[],
+        assets = ["assets/favicon.ico"; "assets/github_buttons.js"; "assets/custom.css"],
     ),
     pages=[
         "Home" => "index.md",
@@ -18,4 +18,5 @@ makedocs(;
 
 deploydocs(;
     repo="github.com/JuliaConstraints/Garamon.jl",
+    devbranch="main",
 )
